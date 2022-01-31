bc53307c3bbb7603bfac1d49986db28848d5ae34
OYEBODE YUSUF AYODEJI <heryordejy.cloudware@gmail.com>
2022-01-31 17:15:36 +0100

    FormData issue - reset navigation on Logout

--- src/components/custom-drawer.jsx
+++ src/components/custom-drawer.jsx
@@ -15,15 +15,22 @@ import {
   handleHideKycIndicator,
 } from '../context-api/indicator-context/handlers';
 import { handleClearRoot } from '../context-api/root-context/handlers';
+import { useNavigation } from '@react-navigation/native';
 
 export default function CustomDrawerContent(props) {
+  const navigation = useNavigation();
+
   const { state: indicatorState, dispatch: indicatorDispatch } = useContext(IndicatorContext);
   const { state: rootState, dispatch: rootDispatch } = useContext(RootContext);
 
   const onLogout = async () => {
-    await indicatorDispatch(handleHideKycIndicator());
+    // await indicatorDispatch(handleHideKycIndicator());
     await indicatorDispatch(handleHideIndicator());
     await rootDispatch(handleClearRoot());
+    await navigation.reset({
+      index: 0,
+      routes: [{ name: 'Dashboard' }],
+    });
   };
 
   return (
